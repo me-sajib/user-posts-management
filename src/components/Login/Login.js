182dcdc3f8765cc4e37c9a5b59168a242bc9651b
sajib <me.mrsajib@gmail.com>
2022-06-03 17:11:00 +0600

    add form tag

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,27 +2,38 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Login = () => {
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(email, password);
+  };
   return (
     <div className="container-width">
       <div class="card w-96 bg-base-100 shadow-xl">
         <div class="card-body">
           <h2 class="card-title text-3xl text-blue-500">Login Now!</h2>
           {/* login form input field */}
-          <input
-            type="text"
-            name="email"
-            placeholder="Type here"
-            class="input input-bordered w-full max-w-xs"
-          />
-          <input
-            type="text"
-            name="password"
-            placeholder="Type here"
-            class="input input-bordered w-full max-w-xs"
-          />
-          <div class="card-actions justify-center">
-            <button class="btn btn-primary">Login</button>
-          </div>
+          <form onSubmit={handleLogin}>
+            <input
+              type="text"
+              name="email"
+              placeholder="Enter your email"
+              class="input input-bordered w-full max-w-xs"
+            />
+            <input
+              type="password"
+              name="password"
+              placeholder="Enter your password"
+              class="input input-bordered w-full max-w-xs"
+            />
+            <div class="card-actions justify-center">
+              <button type="submit" class="btn btn-primary">
+                Login
+              </button>
+            </div>
+          </form>
+
           <p>
             Have No Account?{" "}
             <Link to="/register" className="underline text-blue-500">
