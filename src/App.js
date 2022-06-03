45a712c3e02955448919c3495e9c56ac0ebb7479
sajib <me.mrsajib@gmail.com>
2022-06-03 19:26:49 +0600

    navbar page add

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 import Login from "./components/Login/Login";
 import Registration from "./components/Login/Registration";
 import Users from "./components/Users";
+import Navbar from "./Layouts/Navbar";
 import PrivateRoute from "./Routes/PrivateRoute";
 function App() {
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Login />}></Route>
         <Route path="/register" element={<Registration />}></Route>
