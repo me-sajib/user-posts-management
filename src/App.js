225fcda8d8caf87161a38cd6e335e6ae26ecd0e4
sajib <me.mrsajib@gmail.com>
2022-06-03 18:12:32 +0600

    user page protected

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import "./App.css";
 import Login from "./components/Login/Login";
 import Registration from "./components/Login/Registration";
 import Users from "./components/Users";
+import PrivateRoute from "./Routes/PrivateRoute";
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Login />}></Route>
         <Route path="/register" element={<Registration />}></Route>
-        <Route path="/users" element={<Users />}></Route>
+        <Route
+          path="/users"
+          element={
+            <PrivateRoute>
+              <Users />
+            </PrivateRoute>
+          }
+        ></Route>
       </Routes>
     </>
   );
