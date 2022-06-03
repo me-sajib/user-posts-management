408963af89a329320a44063395338639cb7c40ee
sajib <me.mrsajib@gmail.com>
2022-06-03 16:57:17 +0600

    route set

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Login from "./components/Login/Login";
+import Registration from "./components/Login/Registration";
 import Users from "./components/Users";
 function App() {
   return (
     <>
-      <Users />
-      <Login />
+      <Routes>
+        <Route path="/" element={<Login />}></Route>
+        <Route path="/register" element={<Registration />}></Route>
+        <Route path="/users" element={<Users />}></Route>
+      </Routes>
     </>
   );
 }
