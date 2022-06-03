168d838c1fab67325049808f71bf410b727c76f0
sajib <me.mrsajib@gmail.com>
2022-06-03 18:10:09 +0600

    private route set

---
+++ src/Routes/PrivateRoute.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import auth from "../config/firebase.init";
+import { useAuthState } from "react-firebase-hooks/auth";
+
+const PrivateRoute = ({ children }) => {
+  const [user, loading] = useAuthState(auth);
+  const location = useLocation();
+  if (loading) {
+    return <div>loading......</div>;
+  }
+  if (!user) {
+    return <Navigate to="/login" state={{ from: location }} replace={true} />;
+  }
+  return children;
+};
+
+export default PrivateRoute;
