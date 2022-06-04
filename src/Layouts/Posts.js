1da8e03035235506ae06ac6f769c66ff75033b5d
sajib <me.mrsajib@gmail.com>
2022-06-04 16:00:38 +0600

    fetch data to server and show the dynamic post

--- src/Layouts/Posts.js
+++ src/Layouts/Posts.js
@@ -4,16 +4,17 @@ const Posts = () => {
   const [post, setPosts] = useState([]);
 
   useEffect(() => {
-    fetch("service.json")
+    fetch("http://localhost:5000/posts")
       .then((res) => res.json())
       .then((data) => setPosts(data));
   }, []);
   return (
     <div className="container-width py-12">
       {post.map((post) => (
-        <div class="card w-[100%] bg-base-100 shadow-xl my-12">
+        <div key={post._id} class="card w-[100%] bg-base-100 shadow-xl my-12">
           <div class="card-body">
-            <h2 class="card-title">{post.name}</h2>
+            <h2 class="card-title">{post.title}</h2>
+            <span className="text-gray-500 text-xs">post by {post.postBy}</span>
             <p>{post.description}</p>
           </div>
           <figure>
