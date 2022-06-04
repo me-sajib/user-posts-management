e38eac85ac375f1e3e55251caeeb3b04dbeec632
sajib <me.mrsajib@gmail.com>
2022-06-04 16:19:24 +0600

    add route to user show her all posts

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Login from "./components/Login/Login";
 import Registration from "./components/Login/Registration";
+import MyPosts from "./components/Profile/MyPosts";
 import Users from "./components/Users";
 import Home from "./Layouts/Home";
 import Navbar from "./Layouts/Navbar";
@@ -15,6 +16,7 @@ function App() {
         <Route path="/" element={<Login />}></Route>
         <Route path="/home" element={<Home />}></Route>
         <Route path="/profile" element={<Profile />}></Route>
+        <Route path="/posts" element={<MyPosts />}></Route>
         <Route path="/register" element={<Registration />}></Route>
         <Route
           path="/users"
