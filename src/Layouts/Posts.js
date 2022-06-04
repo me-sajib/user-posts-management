7799213065284527e9ba5266156326b8c213ba0f
sajib <me.mrsajib@gmail.com>
2022-06-04 15:12:03 +0600

    show user post

--- src/Layouts/Posts.js
+++ src/Layouts/Posts.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Posts = () => {
-  return <div></div>;
+  const [post, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("service.json")
+      .then((res) => res.json())
+      .then((data) => setPosts(data));
+  }, []);
+  return (
+    <div className="container-width py-12">
+      {post.map((post) => (
+        <div class="card w-[100%] bg-base-100 shadow-xl my-12">
+          <div class="card-body">
+            <h2 class="card-title">{post.name}</h2>
+            <p>{post.description}</p>
+          </div>
+          <figure>
+            <img src={post.image} alt="Shoes" />
+          </figure>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Posts;
