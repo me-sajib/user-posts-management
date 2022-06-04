c957823339eb45159ea9022cf5e6c25c1087606f
sajib <me.mrsajib@gmail.com>
2022-06-04 20:22:49 +0600

    show all the user post in profile pages

--- src/components/Profile/MyPosts.js
+++ src/components/Profile/MyPosts.js
@@ -1,22 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../config/firebase.init";
 
 const MyPosts = () => {
+  const [user, loading] = useAuthState(auth);
+  const [post, myPost] = useState([]);
+  const userEmail = user?.email;
+  useEffect(() => {
+    fetch(`http://localhost:5000/posts/${userEmail}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => myPost(data));
+  }, [user]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  console.log(post);
   return (
-    <div className="container-width">
-      <div class="card w-96 bg-base-100 shadow-xl image-full">
-        <figure>
-          <img
-            src="https://api.lorem.space/image/shoes?w=400&h=225"
-            alt="Shoes"
-          />
-        </figure>
-        <div class="card-body">
-          <h2 class="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div class="card-actions justify-end">
-            <button class="btn btn-primary">Buy Now</button>
+    <div className="container-width py-12">
+      <div className="grid lg:grid-cols-3 sm:grid-cols-1 md:grid-cols-2 gap-10">
+        {/* map the all post */}
+        {post.map((post) => (
+          <div class="card w-96 bg-base-100 shadow-xl image-full">
+            <figure>
+              <img src={post.image} alt="" />
+            </figure>
+            <div class="card-body">
+              <h2 class="card-title">{post.title}</h2>
+              <p>{post.description}</p>
+              <div class="card-actions justify-end">
+                <button class="btn btn-primary">Delete now</button>
+              </div>
+            </div>
           </div>
-        </div>
+        ))}
       </div>
     </div>
   );
