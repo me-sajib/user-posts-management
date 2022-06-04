468de6e0be6b2bb0854eebdcc535b01ec53845db
sajib <me.mrsajib@gmail.com>
2022-06-04 14:57:24 +0600

    home components

---
+++ src/Layouts/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Banner from "./Banner";
+import Posts from "./Posts";
+
+const Home = () => {
+  return (
+    <div>
+      <Banner />
+      <Posts />
+    </div>
+  );
+};
+
+export default Home;
